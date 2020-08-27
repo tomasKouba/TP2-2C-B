4d1cb7c36ebb2b554ea71d85a61bc0685987cff8
TOMAS AUGUSTO KOUBA <36191494@comunidadort.edu.ar>
2020-08-26 22:06:48 -0300

    update

--- basic/ejerciciosResueltos/6-savetheprisioner.js
+++ basic/ejerciciosResueltos/6-savetheprisioner.js
@@ -7,9 +7,18 @@ function saveThePrisioner(n, m, s) {
     }
     //console.log(prisioneros);
     //console.log(caramelos);
-    console.log(s + caramelos - 1);
+    return (s + caramelos - 1);
 }
 
-saveThePrisioner(5, 2, 1);
-saveThePrisioner(7, 19, 2);
-saveThePrisioner(3, 7, 3);
+console.log(saveThePrisioner(5, 2, 1));
+console.log(saveThePrisioner(7, 19, 2));
+console.log(saveThePrisioner(3, 7, 3));
+
+console.log(saveThePrisioner(1,1,1) === 1);
+console.log(saveThePrisioner(5,2,1) === 2);
+console.log(saveThePrisioner(5,2,2) === 3);
+console.log(saveThePrisioner(3,7,3) === 3);
+console.log(saveThePrisioner(7,19,2) === 6);
+console.log(saveThePrisioner(352926151, 380324688, 94730870) === 122129406);
+console.log(saveThePrisioner(352926151, 380324688, 94730870) === 122129406);
+console.log(saveThePrisioner(353972922, 573651462, 244520504) === 110226121);
